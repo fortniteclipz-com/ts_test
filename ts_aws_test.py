1b4d499c5035d9015e98fac5d02f1586cdb6cae4
Sachin <sachinahj@gmail.com>
2018-08-21 16:40:08 -0700

    test ish

--- ts_aws_test.py
+++ ts_aws_test.py
@@ -1,13 +1,23 @@
 import ts_aws.dynamodb.stream
+import ts_aws.dynamodb.stream_segment
 
 
-stream_segments = ts_aws.dynamodb.stream.get_stream_segments(285219394)
+ss = ts_aws.dynamodb.stream_segment.StreamSegment(
+    stream_id=623,
+    segment=6,
+    padded="006",
+    time_duration=17,
+    time_in=6,
+    time_out=23,
+    url_media_raw="www.twitch-stitch.com",
+    _status=ts_aws.dynamodb.stream_segment.StreamSegmentStatus.FRESHED
+)
 
+freshed = ts_aws.dynamodb.stream_segment.StreamSegmentStatus.FRESHED
+downloaded = ts_aws.dynamodb.stream_segment.StreamSegmentStatus.DOWNLOADED
+print("downloaded", downloaded)
+print("freshed", freshed)
+print("downloaded >= freshed", downloaded >= freshed)
+print("freshed >= downloaded", freshed >= downloaded)
 
-# print("\n\n")
-# for ss in stream_segments:
-#     print(ss.__dict__)
-#     print("\n")
-
-
-print("len(ss)", len(stream_segments))
+print("ss", ss.__dict__)
