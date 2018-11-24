e9bd294cb211a203ea931bffab9c762bcc77f9f1
Sachin Ahuja <sachinahj@gmail.com>
2018-11-24 02:47:17 -0600

    update refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,4 +1,6 @@
 twitch_stitch_root="${PWD%/*/*}"
+
+# purge sqs
 qs=(
     "ts-clip-dead-dev"
     "ts-clip-dev"
@@ -19,5 +21,19 @@ for q in ${qs[@]}; do
     aws sqs purge-queue --queue-url "${qurl}"
 done
 
+# empty s3
 echo "emptying s3 twitch-stitch-media-dev..."
 aws s3 rm s3://twitch-stitch-media-dev --recursive
+
+# drop and create db
+cd $twitch_stitch_root/ts_infra/database
+if [ ! -d ./venv ]; then
+    rm -rf ./venv
+    rm -rf ./__pycache__
+    virtualenv ./venv -p /usr/local/bin/python3
+    source venv/bin/activate
+    pip3 install --process-dependency-links -r requirements.txt
+    deactivate
+fi
+source venv/bin/activate
+alembic downgrade base && alembic upgrade head
