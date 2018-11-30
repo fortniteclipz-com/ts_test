c43079616086415d0eb223fd15a6b96de428e3d8
Sachin Ahuja <sachinahj@gmail.com>
2018-11-29 23:20:18 -0800

    disable and renable rultes

--- refresh/refresh.sh
+++ refresh/refresh.sh
@@ -1,3 +1,6 @@
+aws events disable-rule --name `aws events list-rules --name-prefix ts-jobs-dev | jq -r '.Rules[0].Name'`
 ./sqs.sh
 ./s3.sh
 ./rds.sh
+aws events enable-rule --name `aws events list-rules --name-prefix ts-jobs-dev | jq -r '.Rules[0].Name'`
+
