64294651c056fed7167092672f8dc07ef9df07a8
Sachin Ahuja <sachinahj@gmail.com>
2019-02-26 23:28:58 -0800

    fix bootstrap and test

--- modules/bootstrap.sh
+++ modules/bootstrap.sh
@@ -9,38 +9,48 @@ function bootstrap() {
     rm -rf ./__pycache__
     virtualenv ./venv -p /usr/local/bin/python3
     source venv/bin/activate
-    pip3 install --process-dependency-links -r requirements.txt
+    pip3 install -r requirements.txt
 
     cd $twitch_stitch_root/ts_shared
-    pip3 install --process-dependency-links -e ./ts_aws
-    pip3 install --process-dependency-links -e ./ts_config
-    pip3 install --process-dependency-links -e ./ts_file
-    pip3 install --process-dependency-links -e ./ts_http
-    pip3 install --process-dependency-links -e ./ts_libs
-    pip3 install --process-dependency-links -e ./ts_logger
-    pip3 install --process-dependency-links -e ./ts_model
+    pip3 install -e ./ts_aws
+    pip3 install -e ./ts_config
+    pip3 install -e ./ts_file
+    pip3 install -e ./ts_http
+    pip3 install -e ./ts_libs
+    pip3 install -e ./ts_logger
+    pip3 install -e ./ts_model
 
     echo "done $module_short"
     echo "\n"
 }
 
+cd $twitch_stitch_root
 if [ -z $1 ]; then
-    cd $twitch_stitch_root
-    find . -path "*/modules/*" -name "venv" -exec rm -rf '{}' +
-    find . -path "*/modules/*" -name "__pycache__" -exec rm -rf '{}' +
-    find $(pwd) -type f -path "*/modules/*" -not -path "*.serverless*" -iname "requirements.txt" -print0 | while IFS= read -r -d $'\0' file; do
+
+    find . -path "*/modules/*" -name "venv" -maxdepth 4 -exec rm -rf '{}' +
+    find . -path "*/modules/*" -name "__pycache__" -maxdepth 4 -exec rm -rf '{}' +
+    find $(pwd) -type f -path "*/modules/*" -not -path "*.serverless*" -maxdepth 4 -iname "requirements.txt" -print0 | while IFS= read -r -d $'\0' file; do
         module_dir=$(dirname $file)
         bootstrap $module_dir
     done
+
 else
+
     repo="$(cut -d'/' -f1 <<<"$1")"
     module="$(cut -d'/' -f2 <<<"$1")"
-    module_dir="$twitch_stitch_root/ts_$repo/modules/$module"
-    if [ -d "$module_dir" ]; then
-        bootstrap $module_dir
-    else
+    found=false
+
+    while IFS= read -d " " module_dir; do
+        if [[ $module_dir == *$repo* ]] && [[ $module_dir == *$module* ]]; then
+            found=true
+            bootstrap $module_dir
+        fi
+    done <<< $(find . -path "*/modules/*" -maxdepth 3 -type d)
+
+    if [ $found = false ]; then
         echo "error: $repo $module is not a valid module dir [$repo/$module]"
     fi
+
 fi
 
 
--- modules/test.sh
+++ modules/test.sh
@@ -2,10 +2,11 @@ twitch_stitch_root="${PWD%/*/*}"
 
 function test() {
     module_short="$(basename "${1%/*/*}")/${1##*/}"
+    echo "$1"
     echo "start $module_short"
 
     cd $1
-    if [ -d "$1/venv" ]; then
+    if [ -d "./venv" ]; then
         source venv/bin/activate
         python3 test.py
         echo "done $module_short"
@@ -16,17 +17,28 @@ function test() {
     echo "\n"
 }
 
+cd $twitch_stitch_root
 if [ -z $1 ]; then
+
     echo "error: no module dir given"
+
 else
+
     repo="$(cut -d'/' -f1 <<<"$1")"
     module="$(cut -d'/' -f2 <<<"$1")"
-    module_dir="$twitch_stitch_root/ts_$repo/modules/$module"
-    if [ -d "$module_dir" ]; then
-        test $module_dir
-    else
+    found=false
+
+    while IFS= read -d " " module_dir; do
+        if [[ $module_dir == *$repo* ]] && [[ $module_dir == *$module* ]]; then
+            found=true
+            test $module_dir
+        fi
+    done <<< $(find . -path "*/modules/*" -maxdepth 3 -type d)
+
+    if [ $found = false ]; then
         echo "error: $repo $module is not a valid module dir [$repo/$module]"
     fi
+
 fi
 
 
