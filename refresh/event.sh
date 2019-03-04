000e4326a6743111af712127925505369ff9c12b
Sachin Ahuja <sachinahj@gmail.com>
2019-03-03 16:51:16 -0800

    logging

--- refresh/event.sh
+++ refresh/event.sh
@@ -1,5 +1,5 @@
 action=${1:-'enable'}
-echo "event | $action"
+echo "event | $action | twitch-stitch-media-dev"
 
 aws events list-rules --name-prefix ts-jobs-dev | jq -c '.Rules[]' | jq -c -r '.Name' | while read name; do
     if [ $action == "disable" ]; then
