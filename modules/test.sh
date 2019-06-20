7b53d23229ba63bd778315953fd96462025b5997
Sachin Ahuja <sachinahj@gmail.com>
2019-06-20 07:39:57 -0700

    testing env stuff

--- modules/test.sh
+++ modules/test.sh
@@ -1,18 +1,25 @@
-twitch_stitch_root="${PWD%/*/*}"
+ts_env=${3:-'dev'}
+region=$([ $ts_env == "prod" ] && echo "us-west-2" || echo "us-east-1")
+echo "test | start | ts_env=$ts_env | region=$region"
+
 
 function test() {
     module_short="$(basename "${1%/*/*}")/${1##*/}"
-    echo "test | start | module=$module_short"
+    echo "test | start | ts_env=$ts_env | region=$region | module=$module_short"
     cd $1
     if [ -d "./venv" ]; then
         source venv/bin/activate
+        export AWS_DEFAULT_PROFILE=sls-fortniteclipz
+        export TS_ENV=$ts_env
+        export AWS_DEFAULT_REGION=$region
         python3 test.py
-        echo "test | done | module=$module_short"
+        echo "test | done | ts_env=$ts_env | region=$region | module=$module_short"
     else
-        echo "test | error | not bootstrapped | module=$module_short"
+        echo "test | error | not bootstrapped | ts_env=$ts_env | region=$region | module=$module_short"
     fi
 }
 
+twitch_stitch_root="${PWD%/*/*}"
 cd $twitch_stitch_root
 if [ -z $1 ]; then
     echo "test | error | repo and module not specified"
@@ -27,7 +34,7 @@ else
         fi
     done <<< $(find $(pwd) -type d -path "*/modules/*" -maxdepth 3)
     if [ $found = false ]; then
-        echo "test | error | invalid | repo=$repo | module=$module"
+        echo "test | error | invalid | ts_env=$ts_env | region=$region | repo=$repo | module=$module"
     fi
 fi
 
