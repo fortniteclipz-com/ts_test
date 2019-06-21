9ba97ef98ac4fda71b7483806b0fd3fe73537dea
Sachin Ahuja <sachinahj@gmail.com>
2019-06-20 20:04:47 -0700

    queue stuff

--- refresh/sqs.sh
+++ refresh/sqs.sh
@@ -4,22 +4,22 @@ region=$([ $ts_env == "prod" ] && echo "us-west-2" || echo "us-east-1")
 echo "sqs | start | ts_env=$ts_env | $region"
 
 qs=(
-    "ts-clip-dead"
-    "ts-clip"
-    "ts-montage-dead"
-    "ts-montage"
-    "ts-stream--analyze-dead"
-    "ts-stream--analyze"
-    "ts-stream--initialize-dead"
-    "ts-stream--initialize"
-    "ts-stream-segment--analyze-dead"
-    "ts-stream-segment--analyze"
-    "ts-stream-segment--download-dead"
-    "ts-stream-segment--download"
+    "ts-clip---$ts_env"
+    "ts-clip---$ts_env---dead"
+    "ts-montage---$ts_env"
+    "ts-montage---$ts_env---dead"
+    "ts-stream--analyze---$ts_env"
+    "ts-stream--analyze---$ts_env---dead"
+    "ts-stream--initialize---$ts_env"
+    "ts-stream--initialize---$ts_env---dead"
+    "ts-stream-segment--analyze---$ts_env"
+    "ts-stream-segment--analyze---$ts_env---dead"
+    "ts-stream-segment--download---$ts_env"
+    "ts-stream-segment--download---$ts_env---dead"
 )
 for q in ${qs[@]}; do
     echo "sqs | purging | q=$q-$ts_env"
-    aws sqs purge-queue --queue-url "https://sqs.$region.amazonaws.com/923755341410/$q-$ts_env" --profile sls-fortniteclipz --region $region
+    aws sqs purge-queue --queue-url "https://sqs.$region.amazonaws.com/923755341410/$q" --profile sls-fortniteclipz --region $region
 done
 
 echo "sqs | done | ts_env=$ts_env | $region"
